a5364a79a361a95c39fe568491120d95392a282c
Rollin8395 <57484812+Rollin8395@users.noreply.github.com>
2022-02-22 12:01:25 +0530

    Add files via upload

--- examples/events.py
+++ examples/events.py
@@ -4,11 +4,23 @@ __author__ = 'vahid'
 
 
 if __name__ == '__main__':
-    mycam = ONVIFCamera('192.168.1.10', 8899, 'admin', 'admin') #, no_cache=True)
-    event_service = mycam.create_events_service()
-    print(event_service.GetEventProperties())
-    
+    mycam = ONVIFCamera('172.16.0.60', 80, 'admin', 'P@55word') #, no_cache=True)
+    #event_service = mycam.create_events_service()
+    #print(event_service.GetEventProperties())
+
     pullpoint = mycam.create_pullpoint_service()
-    req = pullpoint.create_type('PullMessages')
-    req.MessageLimit=100
-    print(pullpoint.PullMessages(req))
+    #req=pullpoint.PullMessages({"Timeout":"PT10S", "MessageLimit":3})
+    #print(req.CurrentTime, '--',req.TerminationTime, req.NotificationMessage)
+
+    #pullpoint=event_service.CreatePullPointSubscription()
+    #print(pullpoint.CurrentTime, '----', pullpoint.TerminationTime,'------',pullpoint.SubscriptionReference)
+
+    event_service = mycam.create_events_service()
+    pps = event_service.CreatePullPointSubscription()
+    plp = mycam.pullpoint.zeep_client.create_service(
+        '{http://www.onvif.org/ver10/events/wsdl}PullPointSubscriptionBinding',
+        pps.SubscriptionReference.Address._value_1)
+    pullmess = pullpoint.PullMessages({"Timeout":"PT10S", "MessageLimit":3})
+    print(pullmess.NotificationMessage)
+
+
